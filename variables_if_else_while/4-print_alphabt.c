697c7a68956ef61be7ee07e5dfb0344c5615dfe6
N4yth <nathan.dupuis31@gmail.com>
2025-02-14 12:44:20 +0100

    4.0 create a C programme that show all alphabet exept e and q and with only putchar

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - display the alphabet without e and q and using only putchar
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int i;
+	for (i=97;i<=122;i++)
+	{
+		if (i != 101 || i !=113 )
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
