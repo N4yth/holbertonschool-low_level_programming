864ce703870a244982af4a00e70dc14a38987b8d
N4yth <nathan.dupuis31@gmail.com>
2025-02-14 13:19:54 +0100

    4.1 use for and continue in the if

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,9 @@ int main(void)
 
 	for (i = 97 ; i <= 122 ; i++)
 	{
-		if (i != 101 || i != 113)
-		{
-			putchar(i);
-		}
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
