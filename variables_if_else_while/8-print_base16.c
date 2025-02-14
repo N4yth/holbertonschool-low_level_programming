8bddc6106a6210f6d44496ac60f7565bd7189071
N4yth <nathan.dupuis31@gmail.com>
2025-02-14 15:03:22 +0100

    8.0 create the C programme that display all caractere of HEXA table

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - display the alphabet without e and q and using only putchar
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97 ; i <= 102 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
