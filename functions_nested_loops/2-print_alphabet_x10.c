a091dde8645f3fa88d2d8281e92be376008a791a
N4yth <nathan.dupuis31@gmail.com>
2025-02-15 13:03:39 +0100

    2.0 create the function that display 10 alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - ...
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int sec_i;
+
+	for (sec_i = 0 ; sec_i < 10 ; sec_i++)
+	{
+		for (i = 97 ; i <= 122 ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
