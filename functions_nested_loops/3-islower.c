917d9f7b52ad88eed87fbb23a6837850f0c2a380
N4yth <nathan.dupuis31@gmail.com>
2025-02-15 13:32:19 +0100

    3.0 create the function that say if the entry is on lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - use to see if the entry is a uppercase letter
+ *
+ * Return: 1 if the entry is a uppercase or 0 in all other situation
+ */
+int _islower(int c)
+{
+	unsigned int var;
+
+	var = c ;
+	if (var < 97 || var > 122)
+		return (0);
+	else 
+		return (1);
+}
