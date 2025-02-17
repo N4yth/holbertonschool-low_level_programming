5f04f4b10fad544d9c0dc54b4fd2aadbb920ab10
N4yth <nathan.dupuis31@gmail.com>
2025-02-17 15:25:51 +0100

    10.0 create the function that add 2 number give

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add 2 number
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
