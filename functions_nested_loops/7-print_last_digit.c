680453f225895529ba26dbf5d0a3f1b4b351fdd3
N4yth <nathan.dupuis31@gmail.com>
2025-02-17 10:31:33 +0100

    7.0 create the function that print and return the last digit of entry

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print and return the last digit of the entry
+ * @num: the number to return his last digit
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int num)
+{
+	int result;
+
+	num = num * ((num > 0) - (num < 0));
+	result = (num % 10);
+	_putchar(result + '0');
+	return (result);
+}
