ca2684aa594c098924cd68cab5d6ca4f7048b7bd
N4yth <nathan.dupuis31@gmail.com>
2025-02-18 15:10:40 +0100

    13.0 create the program that find and sum all multiples of 3 and 5 below 1024

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - find all multiples of 3 and 5 and additionnate all of us below 1024
+ *
+ * Return: nothing
+ */
+void main(void)
+{
+	int i;
+	int sum_result = 0;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if ((i % 3) == 0)
+			sum_result += i;
+		if ((i % 5) == 0)
+			sum_result += i;
+	}
+	printf("%d\n", sum_result);
+}
