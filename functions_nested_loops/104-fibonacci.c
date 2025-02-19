20f12f79457c80aaa14a0483b5a587a6fa235d07
N4yth <nathan.dupuis31@gmail.com>
2025-02-19 09:15:23 +0100

    16.0 create the programme that print 98 fibonacci first number

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - ...
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	double i;
+	double old = 1;
+	double new = 2;
+	double save;
+	
+	printf("1, 2");
+	for (i = 1 ; i < 96 ; i++)
+	{
+		save = old;
+		old = new;
+		new = new + save;
+		printf(", %.0lf", new);
+	}
+	printf("\n");
+	return (0);
+}
