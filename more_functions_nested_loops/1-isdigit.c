58c14886b066132a95a5ec8691d925aeac06d266
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 09:35:47 +0100

    1.0 create the programme that return 1 if it s digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - use to know if a charachter is uppercase or not
+ * @c: the character to test
+ *
+ * Return: 1 if the entry is uppercase 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
