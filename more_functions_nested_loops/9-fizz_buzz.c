23b0d259cd7e409c1c4d32412dc07e3985a59744
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 12:48:50 +0100

    9.1 start at 1 not 0 and remove the last space for i=100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,29 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 100 ; i++)
+	for (i = 1 ; i <= 100 ; i++)
 	{
 		if (i % 3 == 0)
 		{
 			if (i % 5 ==0)
 			{
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			}
 			else 
 			{
-				printf("Fizz ");
+				printf("Fizz");
 			}
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i != 100)
+		       printf(" ");	
 
 	}
 	printf("\n");
