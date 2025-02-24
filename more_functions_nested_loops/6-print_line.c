68115a0d20b7461df0061b3a23f9e60ae9fdf8ca
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 11:01:13 +0100

    6.0 create the programme that print n time the _ character

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - display X times _
+ * @n: the number of times to print _ 
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		_putchar('_');
+	_putchar('\n');
+}
