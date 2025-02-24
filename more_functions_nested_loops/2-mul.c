4e55a90f0c8951001218b134de09537e45c88eda
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 09:45:25 +0100

    2.0 create the programme that multiplie two entry

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplie two number
+ * @a: the first character to multiplie
+ * @b: the second character to multiplie
+ *
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
