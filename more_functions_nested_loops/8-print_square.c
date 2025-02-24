9c4a626658466ca8bea1dcc56b523c1facdf4b3d
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 11:30:25 +0100

    8.0 create the programme

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print a square of n size
+ * @n: the size of the square
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int i;
+	int row;
+
+	for (row = 0 ; row < size ; row++)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
