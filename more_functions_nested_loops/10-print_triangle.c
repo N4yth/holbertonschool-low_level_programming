e91aec8991c502d69a3a84374a964b826269128a
N4yth <nathan.dupuis31@gmail.com>
2025-02-24 13:10:58 +0100

    10.0 create the programme that display a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include"main.h" 
+/**
+ * print_triangle - display a triangle
+ * @size: size of the triangle
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int row;
+	int space;
+	int actual_length;
+
+	for (row = 1 ; row <= size ; row++)
+	{
+		for (space = size - row ; space > 0 ; space--)
+		{
+			_putchar(' ');
+		}
+		for (actual_length = 0 ; actual_length < row ; actual_length++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
