83caaffe4e876ad19d2f182a2e48acd02f2d0813
N4yth <nathan.dupuis31@gmail.com>
2025-02-25 14:50:09 +0100

    9.1 correct betty and count the \0

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include "2-strlen.c"
 /**
- * print_array - print n element of a array of integer
- * @a: the array
- * @n: number of element to be printed
+ * _strcpy - print n element of a array of integer
+ * @dest: the pointer where go the copy
+ * @src: the pointer get copy
  *
- * Return: null
+ * Return: the value of the copy
  */
 char *_strcpy(char *dest, char *src)
 {
 	int length, i;
-	
+
 	length = _strlen(src);
-	for (i = 0 ; i < length ; i++)
+	for (i = 0 ; i <= length ; i++)
 	{
 		dest[i] = src[i];
 	}
