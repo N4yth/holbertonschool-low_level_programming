ae1cc7f56b40de93faed8772a815b2af759b8a40
N4yth <nathan.dupuis31@gmail.com>
2025-03-03 15:17:29 +0100

    5.1 correct betty plus the variable

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, sec_i, length_acc, length, count;
+	int i, sec_i, length_nee, length, count;
 
 	length = _strlen(haystack);
 	length_nee = _strlen(needle);
