507f82896561e0a99280b36638f3e9a734ac2d0e
N4yth <nathan.dupuis31@gmail.com>
2025-03-03 14:37:36 +0100

    4.3 increment in the string with the first for loop

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,7 @@
 #include"main.h"
 #include "strlen.c"
+#include <stdio.h>
+
 /**
  * _strpbrk - look into a array to see if there is a character from the list
  * @s: the array to look into
@@ -9,21 +11,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int length, i, sec_i, length_acc;
+	int sec_i, length_acc;
 	
-	length = _strlen(s);
 	length_acc = _strlen(accept);
-	for (i = 0 ; i < length ; i++)
+	for (; *s ; s++)
 	{
 		for (sec_i = 0 ; sec_i < length_acc ; sec_i++)
 		{
-			if (s[i] == accept[sec_i])
+			if (*s == accept[sec_i])
 			{
-				return (&s[i]);
+				return (s);
 			}
 		}
 	}
-	if (*accept != '\0')
-		return ('\0');
 	return (s);
 }
