b4f9cfcea97af5be11e907be807761658b959921
N4yth <nathan.dupuis31@gmail.com>
2025-03-03 10:23:31 +0100

    2.0 create the programme that look for a certain character in array

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include "strlen.c"
+/**
+ * _strchr - look into a array to see if there is a the given chracter
+ * @s: the array to look into
+ * @c: the character to look
+ *
+ * Return: NULL if the character is not found and overwise the adresse
+ * of the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i, length;
+
+	length = _strlen(s);
+	for (i = 0 ; i < length ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return ('\0');
+}
