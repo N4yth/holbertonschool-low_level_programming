adb63b4145e7a015750e32831a0d3e1ac92fc238
N4yth <nathan.dupuis31@gmail.com>
2025-03-10 10:22:47 +0100

    1.0 create the function that cpy an array to an other

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_array - create an array
+ * @c: the char use to fill the array
+ * @size: the size of array
+ *
+ * Return: the array wanted
+ */
+char *_strdup(char *str)
+{
+	int i, size;
+	char *cpy_arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = strlen(str);
+	cpy_arr = malloc(sizeof(char) * size);
+	if (cpy_arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		cpy_arr[i] = str[i];
+	}
+	return (cpy_arr);
+}
