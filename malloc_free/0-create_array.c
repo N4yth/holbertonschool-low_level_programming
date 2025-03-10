69f70781841e943db0da236b257a90a9a1cf28f1
N4yth <nathan.dupuis31@gmail.com>
2025-03-10 09:42:21 +0100

    0.0 create the first project and the other

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array
+ * @c: the char use to fill the array
+ * @size: the size of array
+ *
+ * Return: the array wanted
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr_result;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr_result = malloc(size);
+	for (i = 0 ; i < size ; i++)
+	{
+		arr_result[i] = c;
+	}
+	return (arr_result);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+
+#endif
