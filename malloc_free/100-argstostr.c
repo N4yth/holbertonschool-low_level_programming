9ce3d9aaa3a120361dcfb5ceded24c35341ff5ce
N4yth <nathan.dupuis31@gmail.com>
2025-03-11 10:17:21 +0100

    100.0 create the function that concatenate all argument given

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include "strlen.c"
+#include <stdio.h>
+
+/**
+ * free_grid - free a grid
+ * @grid: the grid to free
+ * @height: height of the grid
+ *
+ * Return: nothing
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str_out;
+	int i, prog = 0, sec_i, count = 0, size;
+	
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < ac ; i++)
+	{
+		count += _strlen(av[i]) + 1;
+	}
+	str_out = malloc(count * sizeof(char));
+	for (i = 0 ; i < ac ; i++)
+	{
+		size = _strlen(av[i]);
+		for (sec_i = 0 ; sec_i < size ; sec_i++)
+		{
+			str_out[prog] = av[i][sec_i];
+			prog++;
+		}
+		str_out[prog++] = '\n';
+	}
+	return (str_out);
+}
