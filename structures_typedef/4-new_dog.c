93e70ee56e117e936c7d0e288576954b3661ba6b
N4yth <nathan.dupuis31@gmail.com>
2025-03-12 13:16:13 +0100

    4.4 save the two variable

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,14 +10,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *name_save, *owner_save;
 	dog_t *other_dog;
+
+	name_save = malloc(sizeof(char *));
+	if  (name_save == NULL)
+	{
+		free(name_save);
+		return (NULL);
+	}
+	owner_save = malloc(sizeof(char *));
+	if (owner_save == NULL)
+	{
+		free(name_save);
+		free(owner_save);
+		return (NULL);
+	}
+	*name_save = *name;
+	*owner_save = *owner;
 
 	other_dog = malloc(sizeof(dog_t));
 	if (other_dog == NULL)
 	{
 		free(other_dog);
-		free(name);
-		free(owner);
+		free(name_save);
+		free(owner_save);
 		return (NULL);
 	}
 	other_dog->name = name;
