7565e90c1c1834673f5ac9138bedc3a577a38a0c
N4yth <nathan.dupuis31@gmail.com>
2025-03-12 10:31:50 +0100

    2.1 correct output for owner

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,8 +18,8 @@ void print_dog(struct dog *d)
 		}
 		printf("age: %f\n", d->age);
 		if (d->owner != NULL)
-			printf("Name: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
-			printf("Name: (nil)\n");
+			printf("Owner: (nil)\n");
 	}
 }
