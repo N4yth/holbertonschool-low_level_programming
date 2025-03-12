408f513400d0bf902de05f68cd34ee179375d093
N4yth <nathan.dupuis31@gmail.com>
2025-03-12 11:34:10 +0100

    4.2 handle malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *other_dog;
 
 	other_dog = malloc(sizeof(char *) + sizeof(float) + sizeof(char *));
-
+	if (other_dog == NULL)
+	{
+		free(other_dog);
+		return (NULL);
+	}
 	other_dog->name = name;
 	other_dog->age = age;
 	other_dog->owner = owner;
