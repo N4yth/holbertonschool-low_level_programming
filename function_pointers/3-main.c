ecec945c0027b4d191dd74403b2ae85f69a3cc21
N4yth <nathan.dupuis31@gmail.com>
2025-03-17 14:50:13 +0100

    3.1 correct some case error

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]), b = atoi(argv[3]);
+	int a, b;
 	int result;
 
 	if (argc != 4)
@@ -28,9 +28,11 @@ int main(int argc, char *argv[])
 	}
 	if (get_op_func(argv[2]) == NULL)
 	{
-		printf("Error3\n");
+		printf("Error\n");
 		exit(99);
 	}
+	a =  atoi(argv[1]);
+	b = atoi(argv[3]);
 	result = (get_op_func(argv[2])(a, b));
 	printf("%d\n", result);
 	return (0);
