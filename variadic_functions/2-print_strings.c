559a0dee6d4a1ab7399dcbf8b0e82a2c202e45ff
N4yth <nathan.dupuis31@gmail.com>
2025-03-18 10:18:19 +0100

    2.0 create the function that print all char parameter

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print char parameter given
+ * @separator: the character use to separate the element print
+ * @n: the number of parameter
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		str = va_arg(args, char*);
+		if (str[0] == '\0')
+		{
+			str = "(nil)";
+		}
+		printf("%s", str);
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
