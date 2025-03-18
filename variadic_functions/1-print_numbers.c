af4dc40d453680f8272beba5e40d4b432d758a47
N4yth <nathan.dupuis31@gmail.com>
2025-03-18 10:01:03 +0100

    1.1 correct the case where the separator given is equal to NULL

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,17 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
 	{
-		va_start(args, n);
-
-		for (i = 0 ; i < n ; i++)
+		printf("%d", va_arg(args, int));
+		if (i != n - 1 && separator != NULL)
 		{
-			printf("%d", va_arg(args, int));
-			if (i != n - 1)
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
