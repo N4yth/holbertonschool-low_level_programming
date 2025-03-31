e64e0c680f649e26fb54a3e7283117743825fa2a
N4yth <nathan.dupuis31@gmail.com>
2025-04-01 00:04:38 +0200

    fix: use old function to add a node one the first and last place

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "1-dlistint_len.c"
+
+/**
+ * retur_to_head - return to the head of a doubly linked list
+ *
+ * @h: the doubly linled list where we are
+ * @idx: the index of the position
+ *
+ * Return: nothing
+ */
+
+void retur_to_head(dlistint_t **h, unsigned int idx)
+{
+	while (idx)
+	{
+		*h = (*h)->prev;
+		idx--;
+	}
+}
+
 /**
  * insert_dnodeint_at_index - add a new node the the choosen coordinate
  *
@@ -14,15 +34,24 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *tmp_prev;
-	dlistint_t *tmp_next;
+	dlistint_t *tmp_prev, *tmp_next;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
 	if (*h == NULL)
-	{
 		return (0);
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+		return (*h);
 	}
-	while (idx - 1 != i) /* go to the previous node */
+	if (dlistint_len(*h) == idx)
+	{
+		add_dnodeint_end(h, n);
+		return (*h);
+	}
+	while (idx > i)
 	{
 		i++;
 		if ((*h)->next == NULL)
@@ -31,24 +60,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		*h = (*h)->next;
 	}
-
-	/* save the next and previous node*/
-	tmp_prev = *h;
-	tmp_next = (*h)->next;
-
-	/* set the next node to the wanted new and swith to it */
-	(*h)->next = new_node;
-	*h = (*h)->next;
-
-	/* set all the value of the new node with the saved one */
+	tmp_prev = (*h)->prev;
+	tmp_next = *h;
+	(*h)->prev->next = new_node;
+	*h = new_node;
 	(*h)->n = n;
 	(*h)->prev = tmp_prev;
 	(*h)->next = tmp_next;
-
-	/* set the h to the head of the doubly linked list */
-	for (i++ ; i ; i--)
-	{
-		*h = (*h)->prev;
-	}
+	retur_to_head(h, i);
 	return (new_node);
 }
+
+
