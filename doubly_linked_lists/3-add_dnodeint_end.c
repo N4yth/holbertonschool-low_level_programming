81941765bbf79b80a28a487460dd9b928fe7bef1
N4yth <nathan.dupuis31@gmail.com>
2025-03-31 17:47:00 +0200

    feat: create the function that create a new tail in a doubly linked list

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add_dnodeint_end - the function create a new tail of a doubly linked list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the int of the new tail
+ *
+ * Return: the new tail of the doubly linked list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_tail = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *head;
+
+	if (new_tail == NULL)
+	{
+		return (NULL);
+	}
+
+	new_tail->n = n;
+	new_tail->next = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_tail;
+	}
+	else
+	{
+		*head = new_tail;
+		return (*head);
+	}
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new_tail;
+	new_tail->prev = tmp;
+	return (*head);
+}
