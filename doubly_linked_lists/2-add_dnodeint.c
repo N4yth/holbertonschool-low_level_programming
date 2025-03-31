60bba1856ac69f9c6312be02cc3182cb66c11e18
N4yth <nathan.dupuis31@gmail.com>
2025-03-31 17:14:13 +0200

    feat: create a function that create a new head for a doubly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add_dnodeint - the function create a new head of a doubly linked list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the int of the new head
+ *
+ * Return: the new head of the doubly linked list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_head = malloc(sizeof(dlistint_t));
+
+	if (new_head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_head->n = n;
+	new_head->prev = NULL;
+	new_head->next = *head;
+
+	*head = new_head;
+
+	return (*head);
+}
