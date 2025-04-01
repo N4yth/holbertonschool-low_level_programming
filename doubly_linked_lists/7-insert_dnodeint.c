feb93091eca155c2778161551b31a6611ed6a6ed
N4yth <nathan.dupuis31@gmail.com>
2025-04-01 10:58:41 +0200

    fix: remove tmp variable that is actualy useless

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,10 +15,10 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp_prev, *tmp_next, *current = *h;
+	dlistint_t *current = *h;
 	dlistint_t *new_node;
 
-	if (*h == NULL || dlistint_len(*h) < idx)
+	if (h == NULL || dlistint_len(*h) < idx)
 		return (NULL);	
 	if (idx == 0)
 	{
@@ -38,12 +38,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node  = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	tmp_prev = current->prev;
-	tmp_next = current;
-	current->prev->next = new_node;
-	current = new_node;
-	current->n = n;
-	current->prev = tmp_prev;
-	current->next = tmp_next;
+	new_node->next = current->next;
+	new_node->prev = current;
+	new_node->n = n;
+	current->next = new_node;
 	return (new_node);
 }
