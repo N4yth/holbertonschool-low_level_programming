4cecf4d52261d5c4b1d3d033efab2efb85eab515
N4yth <nathan.dupuis31@gmail.com>
2025-04-01 12:54:29 +0200

    fix & style: fix the return value to the next node in 3-... and remove whitespace l22 in 7-...

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 
 	if (h == NULL || dlistint_len(*h) < idx)
-		return (NULL);	
+		return (NULL);
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
