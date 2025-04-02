da3a7d241fa47a7ba0cbe1c8caf3dde93e53aea1
N4yth <nathan.dupuis31@gmail.com>
2025-04-02 16:54:25 +0200

    fix: change the position of the flag and change the flog when create/open the file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fb = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fb = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (text_content != NULL)
 	{
