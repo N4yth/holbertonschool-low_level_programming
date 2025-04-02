7fb84ce8fde1ac135e4553aab8e53699962e0a01
N4yth <nathan.dupuis31@gmail.com>
2025-04-02 13:46:08 +0200

    feat: create the function that read and print a file given

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read and write a texte file
+ *
+ * @filename: the name of the file to read
+ * @letters: the number of letter to print
+ *
+ * Return: the number of char printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fb;
+	ssize_t num;
+	char buf[10000];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fb = open(filename, O_RDONLY, 0777);
+	if (fb == -1)
+	{
+		return (0);
+	}
+	num = read(fb, buf, letters);
+	if (num == -1)
+	{
+		return (0);
+	}
+	if (write(STDIN_FILENO, buf, num) == -1)
+	{
+		return (0);
+	}
+	if (close(fb) == -1)
+	{
+		return (0);
+	}
+	return (num);
+
+}
