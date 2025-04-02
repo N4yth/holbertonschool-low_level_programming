50e1e2cd25fa983bac481b3f9b6157c3b1f6f99a
N4yth <nathan.dupuis31@gmail.com>
2025-04-02 16:43:32 +0200

    feat: create the function that create and write into a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - create a file and write into it
+ *
+ * @filename: the name of the file to create or read
+ * @text_content: the text to write in the new file
+ *
+ * Return: 1 if the function work as intended overwise return -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fb;
+	size_t byte_content;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fb = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
+	if (text_content != NULL)
+	{
+		while (text_content[byte_content])
+		{
+			byte_content++;
+		}
+		if (fb == -1)
+		{
+			return (-1);
+		}
+		if (write(fb, text_content, byte_content) == -1)
+		{
+			return (-1);
+		}
+	}
+	if (close(fb) == -1)
+	{
+		return (0);
+	}
+	return (1);
+
+}
