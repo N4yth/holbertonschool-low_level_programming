b94614a67015c84a58d22376a525583d09be590c
N4yth <nathan.dupuis31@gmail.com>
2025-04-02 14:16:49 +0200

    fix: change the permission given when open the file for only be read by the owner

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	fb = open(filename, O_RDONLY, 0777);
+	fb = open(filename, O_RDONLY, 0400);
 	if (fb == -1)
 	{
 		return (0);
